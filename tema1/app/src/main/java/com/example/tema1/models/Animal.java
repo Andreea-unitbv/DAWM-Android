a4eba3877cff5b6cd66463ff248d728fbb99ff65
Andreea-unitbv <andreea.musca@student.unitbv.ro>
2023-03-29 19:21:51 +0300

    Created AniamalsFragment and AnimalClass

---
+++ tema1/app/src/main/java/com/example/tema1/models/Animal.java
@@ -0,0 +1,27 @@
+package com.example.tema1.models;
+
+public class Animal {
+    private String name;
+    private String continent;
+
+    public Animal(String name, String continent) {
+        this.name = name;
+        this.continent = continent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+}
